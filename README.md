d4a3f5b1c6b8c0c8898d6267821d5c0e583a6218
xxg3053 <xxg3053@qq.com>
2018-11-14 22:59:23 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Python
+
+## 基础数据类型
+[代码](https://github.com/xxg3053/learn-python/blob/master/lang/base.go)
+- Number 数字（整数、小数）  
+- int 整数  
+- float 浮点数   
+其他语言： 单精度float, 双精度double   
+其他语言：short, int, long   
+- 列表
+```
+[1, 2, -1, 4, 'a', True]
+```
+
+
+## 变量与运算
+
+## 分支、循环、条件与枚举
+
+## 包、模块、函数与变量作用域
+
+## Python函数
+
+## 面向对象
+
+## 正则、JSON
+
+## 高级语法
+
+## 函数式编程
+
+## 爬虫
+
+## Pythonic
---
+++ lang/base.py
@@ -0,0 +1,27 @@
+
+def number():
+    print("type(1): ", type(1)) # int
+    print("type(-1): ", type(-1)) # int
+    print("type(1.1): ", type(1.1)) # float
+    print("type(1 * 1.0): ", type(1 * 1.0)) # float
+    print("type(2/2): ", type(2/2)) # float
+    print("type(2//2): ", type(2//2)) # int 整除
+
+def group():
+    g = [1, 2, -1, 4, 'a', True ]
+    print('列表: ', type(g)) # list
+    print(g[0])
+    print(g[1:3])
+    print(g[-1:]) # last
+    print(g + ['c','d'])
+    # print(g + ['c','d']) # error
+    # print(g * ['c', 'd']) # error
+    print(g * 3)
+
+
+
+
+
+if __name__ == '__main__':
+    number()
+    group()
